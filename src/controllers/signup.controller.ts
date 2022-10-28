9021990c2c28fd97e6e60dafbe7ad8737f0c8358
Angel Motta <angelmotta@gmail.com>
2022-10-28 12:52:32 -0500

    feat: signup handle unhappy path and return token

--- src/controllers/signup.controller.ts
+++ src/controllers/signup.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express';
 import { encryptionPassword } from '../libs/crypto.helpers';
 import {UserModel, IUser} from '../models/user.model';
 import { HydratedDocument } from 'mongoose';
+import { generateToken } from '../libs/jwt.helpers';
 
 export const signupController = async (req: Request, res: Response) => {
     const hashedPassword = await encryptionPassword(req.body.password);
@@ -16,11 +17,24 @@ export const signupController = async (req: Request, res: Response) => {
     const newUser: HydratedDocument<IUser> =  new UserModel(newReqUser);
     console.log(newUser);
 
+    // Insert new User in MongoDB
     try {
         await newUser.save();
     }
     catch(err) {
         console.log(err);
+        res.status(400).json({"message": "Email already registered"});
+        return;
     }
-    res.status(200).json("Signup Success");
+
+    // Generate Token and send Response
+    console.log(`Generate token`);
+    const token = generateToken(newUser.email);
+
+    // Send HTTP Response
+    const response = {
+        "message": "Signup Success",
+        "token": token
+    }
+    res.status(200).json(response);
 }
